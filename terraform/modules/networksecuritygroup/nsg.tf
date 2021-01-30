f0e47b1b7b199fe2961c127e2a17607b963f257e
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2021-01-30 01:51:17 -0500

    Update resource names

--- terraform/modules/networksecuritygroup/nsg.tf
+++ terraform/modules/networksecuritygroup/nsg.tf
@@ -1,17 +1,17 @@
 resource "azurerm_network_security_group" "nsg" {
-  name                = var.application_type-var.resource_type
+  name                = "var.application_type-nsg"
   location            = var.location
   resource_group_name = var.resource_group
 
   security_rule {
-    name                       = var.application_type-var.resource_type-5000
+    name                       = "inbound-secrule"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "5000"
-    source_address_prefix      = var.address_prefix
+    source_address_prefix      = var.address_prefix_test
     destination_address_prefix = "*"
   }
   security_rule {
