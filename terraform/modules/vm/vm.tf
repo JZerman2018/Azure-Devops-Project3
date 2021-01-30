0a9d88ca3b32684e743f209708a83a103fa4240a
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2021-01-30 02:51:34 -0500

    Update SSH key

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -12,7 +12,7 @@ resource "azurerm_network_interface" "test" {
 }
 
 resource "azurerm_linux_virtual_machine" "test" {
-  name                  = test-vm
+  name                  = "test-vm"
   location              = var.location
   resource_group_name   = var.resource_group
   size                  = "Standard_B1s"
