d97b7099aa0343084b072d161a3302178e632883
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2021-01-30 03:42:25 -0500

    Update main.tf resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,65 +1,74 @@
 provider "azurerm" {
-  tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   features {}
 }
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.35.0"
+    }
+  }
+}
+terraform {
   backend "azurerm" {
-    storage_account_name = "tstate$RANDOM"
+    resource_group_name  = "tstate"
+    storage_account_name = "tstate2021"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
-    access_key           = ""
+    access_key           = ACCOUNT_KEY # (VARIABLE)
   }
 }
 module "resource_group" {
-  source               = ".//modules/resource_group"
+  source               = "./modules/resource_group"
   resource_group       = var.resource_group
   location             = var.location
 }
 module "network" {
-  source               = ".//modules/network"
+  source               = "./modules/network"
   address_space        = var.address_space
   location             = var.location
   virtual_network_name = var.virtual_network_name
   application_type     = var.application_type
-  resource_type        = "NET"
+  resource_type        = "VNET"
   resource_group       = module.resource_group.resource_group_name
-  address_prefix       = var.address_prefix
+  address_prefix_test  = var.address_prefix_test
 }
 
 module "nsg-test" {
-  source              = ".//modules/networksecuritygroup"
+  source              = "./modules/networksecuritygroup"
   location            = var.location
   application_type    = var.application_type
   resource_type       = "NSG"
   resource_group      = module.resource_group.resource_group_name
   subnet_id           = module.network.subnet_id_test
-  address_prefix      = var.address_prefix
-  prefix              = var.prefix
+  address_prefix_test = var.address_prefix_test
 }
 module "appservice" {
-  source           = ".//modules/appservice"
+  source           = "./modules/appservice"
   location         = var.location
   application_type = var.application_type
   resource_type    = "AppService"
   resource_group   = module.resource_group.resource_group_name
-  prefix           = var.prefix
 }
 module "publicip" {
-  source           = ".//modules/publicip"
+  source           = "./modules/publicip"
   location         = var.location
-  prefix           = var.prefix
+  application_type = var.application_type
   resource_group   = module.resource_group.resource_group_name
+  resource_type    = "publicip"
 }
 
-module "virtual_machine" {
-  source               = ".//modules/vm"
+module "vm" {
+  source               = "./modules/vm"
   location             = var.location
+  application_type     = var.application_type
+  resource_type        = "VM"
   resource_group       = module.resource_group.resource_group_name
   subnet_id            = module.network.subnet_id_test
   public_ip_address_id = module.publicip.public_ip_address_id
   admin_username       = var.admin_username
-  prefix               = var.prefix
 }
