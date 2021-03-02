34a96df489e0deb446546a23b40cf0939ce4de31
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2021-03-02 02:47:28 -0500

    Remove storage acct key and add .env file

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ terraform {
     storage_account_name = "project3tstate"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
-    access_key           = ACCOUNT_KEY # (VARIABLE)
+    access_key           =  "ACCOUNT_KEY"
   }
 }
 module "resource_group" {
