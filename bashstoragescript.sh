96c0f9cd065440f167c06fed2c15c72cdd101903
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2021-03-01 21:08:37 -0500

    Update scripts for terraform backend storage

--- bashstoragescript.sh
+++ bashstoragescript.sh
@@ -13,6 +13,7 @@ az policy assignment create --policy tagging-policy
 az group create --name $RESOURCE_GROUP_NAME --location eastus
 # Create storage account
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
 # Get storage account key
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
 # Create blob container
