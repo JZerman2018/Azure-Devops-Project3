0816559f82d42db7d06f5312341582abb63bc972
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2021-03-13 01:25:08 -0500

    Remove extra chromedriver line

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -14,7 +14,7 @@ def login (user, password):
     options = ChromeOptions()
     options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome()
     print('Browser started successfully. Navigating to the demo page to login.')
     logging.info('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
